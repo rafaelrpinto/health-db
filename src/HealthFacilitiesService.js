eb8668800561df24f47a985ac5ed42c0dc4d60d2
Rafael P <raprp@posteo.de>
2017-05-22 00:51:10 +0100

    Improved communication with redis multi()

--- src/HealthFacilitiesService.js
+++ src/HealthFacilitiesService.js
@@ -1,6 +1,23 @@
+let redis = require("redis");
+let bunyan = require("bunyan");
+let Promise = require("bluebird");
 // project dependencies
 let EnumGenerator = require('./EnumGenerator')
-let repository = require('./HealthFacilitiesRepository')
+
+// async redis client
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+// logger setup
+let log = bunyan.createLogger({name: "redis-logger"});
+
+// More options on: https://www.npmjs.com/package/redis#rediscreateclient
+let redisClient = redis.createClient();
+
+// Redis error listener
+redisClient.on("error", function(err) {
+  log.error({err: err});
+});
 
 /**
  * Service responsible for operations related to health facilities.
@@ -21,47 +38,93 @@ class HealthFacilitiesService {
    * Saves a health facility.
    * @param  {Object} healthFacility Facility to be saved.
    */
-  async saveFacility(healthFacility) {
+  saveFacility(healthFacility) {
+    // generates enums
     let facilityTypeEnum = this.facilityTypeEnumGenerator.generate(healthFacility.type);
     let openingHoursEnum = this.openingHoursEnumGenerator.generate(healthFacility.openingHours);
 
-    await Promise.all([
-      // adds the facility type to the domain set
-      repository.saveFacilityType(facilityTypeEnum),
-      // adds the opening hours to the domain set
-      repository.saveOpeningHours(openingHoursEnum),
-      // adds the city to out domain
-      repository.associateCityWithState(healthFacility.address.city, healthFacility.address.state)
-    ]);
+    // groups the redis commands
+    let multi = redisClient.multi();
+
+    // adds the opening hours to the domain set
+    multi.zadd(['facility_opening_hours_list', openingHoursEnum.id, `${openingHoursEnum.id}:${openingHoursEnum.description}`]);
+    // adds the facility type to the domain set
+    multi.zadd(['facility_type_list', facilityTypeEnum.id, `${facilityTypeEnum.id}:${facilityTypeEnum.description}`]);
+    // adds the city to out domain
+    multi.sadd(`cities:${healthFacility.address.state}`, healthFacility.address.city);
 
     // array with ids of the services
     let facilityServices = [];
 
     // creates the services indexes
     for (let service of healthFacility.services) {
+      // generates th enum
       let serviceEnum = this.servicesEnumGenerator.generate(service.trim());
 
-      await Promise.all([
-        // adds the service to the domain set
-        repository.saveService(serviceEnum),
-        //adds this facility to the service index
-        repository.associateFacilityWithService(healthFacility, serviceEnum)
-      ]);
+      // adds the service to the domain set
+      multi.zadd(['service_list', serviceEnum.id, `${serviceEnum.id}:${serviceEnum.description}`]);
+
+      //associate with service per state
+      multi.zadd([`service:${serviceEnum.id}:${healthFacility.address.state}`, healthFacility.id, healthFacility.id]);
+      // and also per city to allow more accurate searching
+      multi.zadd([`service:${serviceEnum.id}:${healthFacility.address.state}:${healthFacility.address.city}`, healthFacility.id, healthFacility.id]);
 
       //adds the numeric id to be associated with the hash later
       facilityServices.push(serviceEnum.id);
     }
 
-    // we replace the long strings by the generated enum ids to improve performance
-    let facilityToBeSaved = Object.assign(healthFacility, {
-      type: facilityTypeEnum.id,
-      openingHours: openingHoursEnum.id,
-      services: facilityServices.join(',')
-    });
+    // stores the health facility as hash
+    multi.hmset(`facility:${healthFacility.id}`, hash(healthFacility, facilityTypeEnum, openingHoursEnum, facilityServices));
 
-    // saves the facility
-    return repository.saveFacility(facilityToBeSaved);
+    // adds the health facility to the geographic index
+    multi.geoadd('geo_facilities', healthFacility.longitude, healthFacility.latitude, healthFacility.id);
+
+    // executes all commands in the same request
+    return multi.execAsync();
   }
+}
+
+/**
+ * Generates the hahs argument for a facility.
+ * @param  {[type]} healthFacility   Facility to be stored.
+ * @param  {[type]} facilityTypeEnum Type of the facility.
+ * @param  {[type]} openingHoursEnum Opening hours of the facility.
+ * @param  {[type]} facilityServices Services provided by the facility.
+ * @return {Array}                  Array containing the arguments for hash creation.
+ */
+function hash(healthFacility, facilityTypeEnum, openingHoursEnum, facilityServices) {
+  return [
+    'id',
+    healthFacility.id,
+    'name',
+    healthFacility.name,
+    'businessName',
+    healthFacility.businessName,
+    'address.street',
+    healthFacility.address.street,
+    'address.number',
+    healthFacility.address.number,
+    'address.neighborhood',
+    healthFacility.address.neighborhood,
+    'address.postalCode',
+    healthFacility.address.postalCode,
+    'address.city',
+    healthFacility.address.city,
+    'address.state',
+    healthFacility.address.state,
+    'address.latitude',
+    healthFacility.latitude,
+    'address.longitude',
+    healthFacility.longitude,
+    'type',
+    facilityTypeEnum.id,
+    'phone',
+    healthFacility.phone,
+    'openingHours',
+    openingHoursEnum.id,
+    'services',
+    facilityServices.join(',')
+  ]
 }
 
 module.exports = HealthFacilitiesService;
